4502dc83b0e1c06c41577f3569ca38e29169ed66
Rebecca <angryhamster14@gmail.com>
2018-02-11 15:14:27 -0800

    Converted many features to 3.0

--- MongoDB_Handle_Input.java
+++ MongoDB_Handle_Input.java
@@ -6,28 +6,28 @@ import java.util.Arrays;
 import org.bson.Document;
 
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.client.model.Sorts.*;
 
 
 
 public class MongoDB_Handle_Input implements DB_Handle_Input{
 
 	private MongoClient mongoClient;
-	private DB db;
-	private DBCollection collection;
+	private MongoDatabase db;
+	private MongoCollection<Document> collection;
 	
 	@SuppressWarnings("deprecation")
 	public MongoDB_Handle_Input(String colName) {
 		try {
 			 mongoClient = new MongoClient();
-			 db = mongoClient.getDB("BeakyDB");
+			 db = mongoClient.getDatabase("BeakyDB");
 
 			 collection = db.getCollection(colName);
 	       
@@ -41,61 +41,60 @@ public class MongoDB_Handle_Input implements DB_Handle_Input{
 	
 	@Override
 	public void insertTask(TaskItem t) {
-		DBObject task = new BasicDBObject()
+		Document task = new Document()
                 .append("name", t.name)
                 .append("dueDate",(t.date))
                 .append("priority" , t.priority);
                  
 		
-       collection.insert(task);
+       collection.insertOne(task);
    //    collection.insert({"date":ISODate("2016-03-03T08:00:00.000")});
 		
 	}
 
 	@Override
 	public void removeItem(String taskName) {
-		DBObject query = new BasicDBObject("name", taskName);
-        DBCursor cursor = collection.find(query);
-        collection.remove(cursor.next());
-		// TODO Auto-generated method stub
+		Document query = new Document("name", taskName);
+		MongoCursor<Document> cursor = collection.find(query).iterator();
+        collection.deleteOne(cursor.next());
+		
 		
 	}
 
 	
-	//List<DBObject> all = coll.find().toArray();
+	//List<Document> all = coll.find().toArray();
 	@Override
 	public void printList() {
-         DBCursor cursor = collection.find();
-         while(cursor.hasNext()){
-        	 System.out.println(cursor.next());
-         }
+		printList("1");
          
 		
 	}
 
 	@Override
 	public void printList(String sortedBy) {
-		  DBCursor cursor = collection.find();
+		MongoCursor<Document> cursor = collection.find().iterator();
 		  
 		  //sort by due date
 		  if(sortedBy.equals("2")){
-			  cursor.sort(new BasicDBObject("dueDate", 1));
+			  
+			  cursor = (collection.find().sort(Sorts.orderBy(Sorts.descending("dueDate")))).iterator();
+		//	  cursor.sort(new BasicDBObject("dueDate", 1));
 		  }
 		  
 		  //aggregate to set priorities//TODO: Learn what aggregate does, then convert it w this. Found similar result, copy that
 		  if(sortedBy.equals("3")){
 			  
 			  /*
-			  Iterable<DBObject> output = collection.aggregate(Arrays.asList(
+			  Iterable<Document> output = collection.aggregate(Arrays.asList(
 					  
-					  (DBObject) new BasicDBObject("$project",
-							  (DBObject) new BasicDBObject("$order",
-									  (DBObject) new BasicDBObject("$cond",
-											  (DBObject) new BasicDBObject("$if",
-													  (DBObject) new BasicDBObject("$eq", Arrays.asList("$priority", "very high")))
+					  (Document) new Document("$project",
+							  (Document) new Document("$order",
+									  (Document) new Document("$cond",
+											  (Document) new Document("$if",
+													  (Document) new Document("$eq", Arrays.asList("$priority", "very high")))
 											  .append("then", 1)
 											  .append("else", 2) ))),
-					  (DBObject) new BasicDBObject("$sort", new BasicDBObject("$order", 1)))).results();
+					  (Document) new Document("$sort", new Document("$order", 1)))).results();
 					  */
 			
 			MongoCollection<Document> my_collection = db.getCollection("todoList1");
@@ -111,9 +110,9 @@ public class MongoDB_Handle_Input implements DB_Handle_Input{
 			          ));
 
 			  // Print for demo
-			  for (Document dbObject : output)
+			  for (Document Document : output)
 			  {
-			      System.out.println(dbObject);
+			      System.out.println(Document);
 			  }
 			 
 			  /*
@@ -129,27 +128,27 @@ public class MongoDB_Handle_Input implements DB_Handle_Input{
 				
 			 
 			  /*
-			 DBObject order = (DBObject) new BasicDBObject("$order",
-					  (DBObject) new BasicDBObject("$cond",
-							  (DBObject) new BasicDBObject("$if",
-									  (DBObject) new BasicDBObject("$eq", Arrays.asList("$priority", "very high")))
+			 Document order = (Document) new Document("$order",
+					  (Document) new Document("$cond",
+							  (Document) new Document("$if",
+									  (Document) new Document("$eq", Arrays.asList("$priority", "very high")))
 							  .append("then", 1)
 							  .append("else", 2) ));
 			  AggregationOutput output = collection.aggregate(Arrays.asList(
 					  
-					  (DBObject) new BasicDBObject("$project", order
+					  (Document) new Document("$project", order
 							  ),
-					  (DBObject) new BasicDBObject("$sort", order)));
+					  (Document) new Document("$sort", order)));
 			  System.out.println(output);
 			  */
 					  
 					  
 					  /*
-				        (DBObject) new BasicDBObject("$unwind", "$views"),
-				        (DBObject) new BasicDBObject("$match", new BasicDBObject("views.isActive", true)),
-				        (DBObject) new BasicDBObject("$sort", new BasicDBObject("views.date", 1)),
-				        (DBObject) new BasicDBObject("$limit", 200),
-				        (DBObject) new BasicDBObject("$project", new BasicDBObject("_id", 0)
+				        (Document) new Document("$unwind", "$views"),
+				        (Document) new Document("$match", new Document("views.isActive", true)),
+				        (Document) new Document("$sort", new Document("views.date", 1)),
+				        (Document) new Document("$limit", 200),
+				        (Document) new Document("$project", new Document("_id", 0)
 				                    .append("url", "$views.url")
 				                    .append("date", "$views.date"))
 				        )).results(); */
@@ -180,7 +179,7 @@ public class MongoDB_Handle_Input implements DB_Handle_Input{
 		  while(cursor.hasNext()){
 	        	 System.out.println(cursor.next());
 	      }
-		// TODO Auto-generated method stub
+		
 		
 	}
 	
