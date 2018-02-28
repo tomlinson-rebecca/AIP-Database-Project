ca0e4b87888b8ea6dc1016fed7bd23b94384e945
Rebecca <angryhamster14@gmail.com>
2018-02-27 16:12:45 -0800

    Order by priority implemented

--- PriorityComparator.java
+++ PriorityComparator.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
-//compares priorities of DynamoDB items. -1 means lower priority, 0 equal, 1 higher
+//compares priorities of DynamoDB items. -1 means higher priority, 0 equal, 1 lower
 public class PriorityComparator implements Comparator<Map<String, AttributeValue> > {
 
 	@Override
@@ -17,8 +17,24 @@ public class PriorityComparator implements Comparator<Map<String, AttributeValue
 			}
 			
 			return -1;
-		}
-		
+		} else if (arg0.get("priority").toString().equals("{S: high,}")){
+			if(arg1.get("priority").toString().equals("{S: very high,}")){
+				return 1;
+			} else if (arg1.get("priority").toString().equals("{S: high,}")){
+				return 0;
+			}
+			
+			return -1;
+			
+		} else if (arg0.get("priority").toString().equals("{S: medium,}")){
+			if(arg1.get("priority").toString().equals("{S: very high,}") || arg1.get("priority").toString().equals("{S: high,}")){
+				return 1;
+			} else if (arg1.get("priority").toString().equals("{S: medium,}")){
+				return 0;
+			}
+			
+			return -1;
+		}	
 		// TODO Auto-generated method stub
 		
 		return 1;
